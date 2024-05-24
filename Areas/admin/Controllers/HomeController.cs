e95075ce769a41db29659beb8f484a5d081508a9
sahib memmedov <sahibmemmedov7890@gmail.com>
2024-05-23 19:08:31 -0700

    first commit

---
+++ Areas/admin/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sbs.Areas.admin.Controllers
+{
+    [Area("admin")]
+    /*[Authorize]*/ 
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using sbs.DAL;
+using sbs.Models;
+
+namespace sbs.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Category> categories = await _context.Categories.ToListAsync();
+            return View(categories);
+        }
+    }
+}
